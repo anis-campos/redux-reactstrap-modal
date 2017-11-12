1c1992c6307b9fac1da6269818eddd8cf4d0c75d
Anis DA SILVA CAMPOS <yocorporation@hotmail.com>
2017-11-12 19:36:41 +0100

    Adding reducer tests

--- test/store.reducer.test.js
+++ test/store.reducer.test.js
@@ -1,8 +1,20 @@
 import reducer from '../src/reducer'
 import {normalize} from "normalizr";
 import {dialogSchema} from '../src/schemas'
+import * as actions from '../src/actions'
+import * as c from '../src/constants'
 
-describe('favorite reducer', () => {
+describe('modal reducer', () => {
+
+    const normalizeArray = (obj) => {
+        const map = obj.map((elem) => elem.dialog);
+        return normalize(map, [dialogSchema]).entities;
+    };
+
+    const normalizeOne = (obj) => {
+        const map = obj.map((elem) => elem.dialog);
+        return normalize(map, [dialogSchema]).entities;
+    };
 
 
     it('should return the initial state', () => {
@@ -12,4 +24,35 @@ describe('favorite reducer', () => {
         expect(reducer(undefined, {})).to.deep.equal(initialState)
     });
 
+
+    it('should handle 1 OPEN_DIALOG', () => {
+        const state = reducer(undefined, actions.openDialog('modal-1'));
+        const normalizer = (obj) => normalize(obj, {dialog: dialogSchema}).entities;
+        const normalized = normalizer(actions.openDialog('modal-1'));
+        expect(state).to.deep.equal(normalized);
+    });
+
+    it('should handle 2 OPEN_DIALOG', () => {
+        let state = reducer(undefined, actions.openDialog('modal-1'));
+        state = reducer(state, actions.openDialog('modal-2'));
+        const normalized = normalizeOne([actions.openDialog('modal-1'), actions.openDialog('modal-2')]);
+        expect(state).to.deep.equal(normalized);
+    });
+
+
+    it('should handle 2 OPEN_DIALOG and 1 TOGGLE_DIALOG', () => {
+        let state = reducer(undefined, actions.openDialog('modal-1'));
+        state = reducer(state, actions.openDialog('modal-2'));
+        state = reducer(state, actions.toggleDialog('modal-1'));
+        const normalized = normalizeArray([actions.openDialog('modal-1'), actions.openDialog('modal-2'), actions.closeDialog('modal-1')]);
+        expect(state).to.deep.equal(normalized);
+    });
+
+    it('should handle 2 OPEN_DIALOG and 1 CLOSE_DIALOG', () => {
+        let state = reducer(undefined, actions.openDialog('modal-1'));
+        state = reducer(state, actions.openDialog('modal-2'));
+        state = reducer(state, actions.closeDialog('modal-1'));
+        const normalized = normalizeArray([actions.openDialog('modal-1'), actions.openDialog('modal-2'), actions.closeDialog('modal-1')]);
+        expect(state).to.deep.equal(normalized);
+    });
 });

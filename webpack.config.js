d184ae35631cf0c26e989dd1ebefb3e1a2684fad
Anis DA SILVA CAMPOS <yocorporation@hotmail.com>
2019-04-24 19:49:39 -0400

    travis: node matrix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const OpenBrowserPlugin = require('open-browser-webpack-plugin');

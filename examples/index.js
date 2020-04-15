727df5266a9c53b78bdf329d35f31db2cc7ddba6
Anis <anis@lumapps.com>
2020-04-15 22:33:53 +0200

    Adding github pages - new exemple based un CRA - new npm scripts for deploy - build folder igonred

--- examples/index.js
+++ examples/index.js
@@ -1,49 +1,12 @@
-import React from "react";
-import {render} from "react-dom";
-import reduxDialog, {dialogReducer, openDialog} from "../src";
-import {combineReducers, createStore} from "redux";
-import {Provider,connect} from "react-redux";
-import {Button,ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-
-import "bootstrap/dist/css/bootstrap.min.css";
-
-const reducers = {
-    dialogReducer: dialogReducer
-};
-
-const reducer = combineReducers(reducers);
-const store = createStore(reducer);
-
-const BasicDialog = ({toggle}) => (
-    <div>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-        <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-        </ModalBody>
-        <ModalFooter>
-            <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={toggle}>Cancel</Button>
-        </ModalFooter>
-    </div>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('app')
 );
 
-const Dialog = reduxDialog(connect,{
-    name: 'testDialog'
-})(BasicDialog);
-
-const App = () => (
-    <Provider store={store}>
-        <div>
-            <Dialog />
-            <button onClick={() => store.dispatch(openDialog('testDialog'))}>Open Dialog 2</button>
-        </div>
-    </Provider>
-);
-
-render(<App />,
-    document.getElementById('app'));
